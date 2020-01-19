903b61c1b202ee782bc32e1cc45008a212f35600
Raj Kumar <rachkumr77@gmail.com>
2020-01-19 20:45:18 +0530

    Initial Commit

---
+++ BaseArchitect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaseArchitect
+//
+//  Created by Raj Kumar on 19/01/20.
+//  Copyright © 2020 Raj Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
